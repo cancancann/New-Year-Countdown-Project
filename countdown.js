35af58757401768a71be28b99661c9ab9f299c2e
cancancann <73189111+cancancann@users.noreply.github.com>
2022-06-06 00:11:02 +0300

    first commit

---
+++ countdown.js
@@ -0,0 +1,40 @@
+const DaysEl = document.getElementById('Days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2023';
+
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time; //zaman ayarını ayarlar
+}
+
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    DaysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML =  formatTime(seconds);
+
+    console.log(days,hours,minutes)
+
+}
+
+
+countdown();
+
+
+//Çağrı sürekli
+setInterval(countdown,1000); //milisaniye cinsinden
+
